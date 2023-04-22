d13e8e5da2a443fcaf724fad7b61bcf628400d65
nardo88 <nardo1988@mail.ru>
2023-04-22 13:24:13 +0300

    1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import App from "./App";
 
-test("first", () => {
-  expect(true).toBeTruthy();
+test("Renders main page correctly", async () => {
+  render(<App />);
+  const buttonCount = await screen.findByRole("button");
+
+  // проверяем что значение 0
+  expect(buttonCount.innerHTML).toBe("count is: 0");
+
+  // Делаем 2 щелчка
+  user.click(buttonCount);
+  user.click(buttonCount);
+  screen.debug();
+  // проверяем что текст кнопки изменился
+  expect(buttonCount.innerHTML).toBe("count is: 2");
 });
